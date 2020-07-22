d6e3096b5dfd54ab4d86ac293313a86bce5a4f27
milesuno <milesoluku@gmail.com>
2020-07-22 16:34:36 +0100

    Robot head img added and body re-colored

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import "./App.css";
 import botImg from "./icons/icons8-bot-100.png"
 import List from "./components/list/list";
 
+
+//TODO: Add background
+//TODO: Add randomise comments from BOT when items are added
 //TODO: Add delete and edit functionality
 //TODO: Add audio for adding and removing items
 //TODO: Create employee tag for "To-do" bot.
+
+
 function App() {
 	return (
 		<div className="App">
