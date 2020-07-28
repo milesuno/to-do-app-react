7be93ca9b76d574816c3b0f7901e4e6a2b289b72
milesuno <milesoluku@gmail.com>
2020-07-28 16:15:50 +0100

    Page bg added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import "./App.css";
-import botImg from "./icons/icons8-bot-100.png"
+import botImg from "./icons/icons8-bot-100.png";
 import List from "./components/list/list";
 
-
-//TODO: Add background
 //TODO: Add randomise comments from BOT when items are added
-//TODO: Add delete and edit functionality
 //TODO: Add audio for adding and removing items
 //TODO: Create employee tag for "To-do" bot.
-
 
 function App() {
 	return (
 		<div className="App">
-      <img src={botImg} />
+			<img src={botImg} />
 			<List />
 		</div>
 	);
